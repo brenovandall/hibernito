7ed94dc5cb8568f52d2ecedb56db5e4de1eb63cc
Breno Van Dall <vbreno335@gmail.com>
2025-10-31 22:58:43 -0300

    feats

---
+++ src/Habernito.Api/Domain/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace Habernito.Api.Domain
+{
+    public class Product
+    {
+        public string Name { get; set; } = default!;
+        public string Category { get; set; } = default!;
+        public bool Discontinued { get; set; }
+    }
+}
