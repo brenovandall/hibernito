c35a33a21b7dc229072ecf145c2656d09f3e66b9
Breno Van Dall <vbreno335@gmail.com>
2025-11-01 16:02:30 -0300

    feats

--- src/Hibernito/Domain/Product.cs
+++ src/Hibernito/Domain/Product.cs
@@ -1,7 +1,8 @@
-﻿namespace Habernito.Api.Domain
+﻿namespace Hibernito.Domain
 {
     public class Product
     {
+        public Guid Id { get; set; }
         public string Name { get; set; } = default!;
         public string Category { get; set; } = default!;
         public bool Discontinued { get; set; }
---
+++ tests/Hibernito.Tests/GenerateSchemaFixtureTests.cs
@@ -0,0 +1,24 @@
+﻿using Hibernito.Domain;
+using NHibernate.Cfg;
+using NHibernate.Tool.hbm2ddl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hibernito.Tests
+{
+    public class GenerateSchemaFixtureTests
+    {
+        [Fact]
+        public void NHibernate_CanGenerateSchema_WhenLoaded()
+        {
+            var cfg = new Configuration();
+            cfg.Configure();
+            cfg.AddAssembly(typeof(Product).Assembly);
+
+            new SchemaExport(cfg).Execute(false, true, false);
+        }
+    }
+}
